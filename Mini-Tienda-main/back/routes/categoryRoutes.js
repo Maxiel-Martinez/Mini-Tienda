91fdce28bb570cd4044e9a03a89e4236dc38bea4
Maxiel-Martinez <maxielmartinezc@gmail.com>
2025-11-05 00:07:34 -0500

    Arreglo de vistas

--- Mini-Tienda-main/back/routes/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { CategoryController } from "../controllers/categoryController.js";
-
-
-export const categoryRouter = Router();
-
-categoryRouter.get("/", CategoryController.getAllCategories);
-categoryRouter.get("/:categori_id", CategoryController.getCategoryById);
--- Mini-Tienda-main/back/routes/userRouter.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { UserController } from '../controllers/userController.js'
-
-export const userRouter = Router()
-
-userRouter.post('/login', UserController.loginUser)
-userRouter.post('/register', UserController.createUser)
