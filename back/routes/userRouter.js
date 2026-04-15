0c1129199a60f251021345027733d42c0a9a0a37
DD150602 <dadrdev@gmail.com>
2026-04-15 18:19:49 -0500

    Refactor user authentication flow and enhance error handling in login and logout methods

--- back/routes/userRouter.js
+++ back/routes/userRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/userController.js'
+import { requiresAuth } from '../middlewares/auth.js';
 
 export const userRouter = Router()
 
 userRouter.post('/login', UserController.loginUser)
-userRouter.post('/register', UserController.createUser)
 userRouter.post('/logout', UserController.logoutUser)
+userRouter.get('/me', requiresAuth, UserController.me)
