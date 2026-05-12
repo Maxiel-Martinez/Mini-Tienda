55836d43eeedeaaaf62bfaa78ce6b1c1722bbfb5
DD150602 <dadrdev@gmail.com>
2026-05-12 01:18:46 -0500

    Migrate dashboard page to React profile layout

--- client/src/pages/Profile/components/titleSection/TitleSection.jsx
+++ client/src/pages/Profile/components/titleSection/TitleSection.jsx
@@ -7,12 +7,14 @@ export const TitleSection = ({ title, subtitle, buttonText, onButtonClick }) => 
         <h1>{title}</h1>
         <p>{subtitle}</p>
       </div>
-      <div className="title-right">
-        <button className="btn-nuevo" type="button" onClick={onButtonClick}>
-          <span style={{ fontSize: "18px" }}>+</span>
-          {buttonText}
-        </button>
-      </div>
+      {buttonText ? (
+        <div className="title-right">
+          <button className="btn-nuevo" type="button" onClick={onButtonClick}>
+            <span style={{ fontSize: "18px" }}>+</span>
+            {buttonText}
+          </button>
+        </div>
+      ) : null}
     </div>
   )
 }
